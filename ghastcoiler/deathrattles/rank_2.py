ea2d3e2bc3050bcc191527fe01e52c1ebf68a5fa
danicfortier <danic-soccer@hotmail.com>
2020-05-14 16:23:18 -0400

    Added the core for Rank 2 minions

---
+++ ghastcoiler/deathrattles/rank_2.py
@@ -0,0 +1,42 @@
+import logging
+
+from game.player_board import PlayerBoard
+from minions.base import Minion
+from deathrattles.base import Deathrattle
+from minions.tokens import DamagedGolem, Imp, BigBadWolf, Rat
+
+
+class HarvestGolemDeathrattle(Deathrattle):
+    # TODO
+    pass
+
+
+class ImprisonerDeathrattle(Deathrattle):
+    # TODO
+    pass
+
+
+class KaboomBotDeathrattle(Deathrattle):
+    # TODO
+    pass
+
+
+class KindlyGrandmotherDeathrattle(Deathrattle):
+    # TODO
+    pass
+
+
+class RatPackDeathrattle(Deathrattle):
+    # TODO
+    pass
+
+
+class SpawnofNZothDeathrattle(Deathrattle):
+    # TODO
+    pass
+
+
+class UnstableGhoulDeathrattle(Deathrattle):
+    # TODO
+    pass
+
